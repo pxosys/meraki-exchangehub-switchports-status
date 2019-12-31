d4222dad8a90734522909a62cbba7f789b102827
Matt Smith <matthew@pxosys.com>
2019-12-31 14:46:06 +0000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Setup + Installation
+    1. Download repository and move to desired directory
+    2. Run `pip install -r requirements.txt` to install any potentially missing packages
+    3. Go to the Meraki Dashboard and generate an API key for yourself
+    4. Paste this value into the provided credentials.json file (if desired)
+    5. Run script. The script will message your designated room with check your network for new devices every 5 minutes and message the desginated chat channel if it detects a new device. For instructions on running the script, read below
+
+# Instructions
+The script obtains its credentials in two ways: 
+- Imported from a JSON file using the --credentials argument
+- Specifically designated using the --apiKey argument
+
+To run the script, enter `python[3] main.py [arguments]` and the script will begin, requiring no further input
+
+
+# Description
+The script will output a dict that will list all the switches in your organisations, with each one having a "ports" entry which will have an "inUse" key against it, which will be true if a device is detected to be plugged in to it
---
+++ main.py
@@ -0,0 +1,55 @@
+from classes import Api, Meraki
+import json, time, argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--apiKey', required=False)
+    parser.add_argument('--credentialsFile', required=False)
+
+    args = parser.parse_args()
+
+    apiKey = None
+
+    if args.credentialsFile:
+        with open(args.credentialsFile) as json_file:
+            parsed = json.load(json_file)
+            if "apiKey" in parsed:
+                apiKey = parsed["apiKey"]
+
+    if args.apiKey:
+        apiKey = args.apiKey
+
+    if apiKey == None:
+        print('Please include a Cisco Meraki Dashboard API Key!')
+        exit()
+
+
+    meraki = Meraki(apiKey)
+
+    orgs = meraki.getOrganisations()
+    resultDevices = {}
+    if len(orgs["data"]):
+        for org in orgs["data"]:
+            networks = meraki.getNetworks(org)
+            if len(networks["data"]):
+                for network in networks["data"]:
+                    devices = meraki.getDevices(network)
+                    for device in devices["data"]:
+                        device["ports"] = {}
+                        if device["pxType"] == 'switch':
+                            resultDevices[device["mac"]] = device
+                            ports = meraki.getSwitchPorts(device)
+                            for port in ports["data"]:
+                                port["inUse"] = False
+                                resultDevices[device["mac"]]["ports"]["num" + str(port["number"])] = port
+                            clients = meraki.getDeviceClients(device)
+                            if clients["success"]:
+                                for client in clients["data"]:
+                                    if client["switchport"]:
+                                         resultDevices[device["mac"]]["ports"]["num" + client["switchport"]]["inUse"] = True
+    print(resultDevices)
+
+if __name__ == '__main__':
+    main()
